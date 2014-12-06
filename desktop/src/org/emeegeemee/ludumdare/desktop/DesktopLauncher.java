6d92230195648c39ac236903e32f97055b06eaef
emeegeemee <emeegeemee@gmail.com>
2014-12-05 18:36:51 -0700

    imported libgdx project

---
+++ desktop/src/org/emeegeemee/ludumdare/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.emeegeemee.ludumdare.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.emeegeemee.ludumdare.LudumDare;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumDare(), config);
+	}
+}
